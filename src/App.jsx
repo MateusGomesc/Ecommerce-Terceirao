84c90f54405ef065c147b6cbea33290eb0a08dd8
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-04-13 13:52:56 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Header from './components/layout/Header'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const container = styled.header`
+    padding: 0 32px 0 32px;
+    
+`
+
+export default function Header(){
+    return(
+        <></>
+    )
+}
