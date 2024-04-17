4b779b645f9944520e3a4d2492b690a738512ba9
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-04-17 15:44:22 -0300

    feat: termos de uso

--- src/components/layout/ContainerDefault.style.jsx
+++ src/components/layout/ContainerDefault.style.jsx
@@ -7,7 +7,6 @@ export const ContainerDefault = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
 
     @media(min-width: 1024px){
         & {
--- src/components/pages/Terms.jsx
+++ src/components/pages/Terms.jsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components'
+
+import { Title } from '../layout/TextGradient.style'
+import { ContainerDefault } from '../layout/ContainerDefault.style'
+
+const List = styled.ul`
+    font-family: inherit;
+    font-size: 16px;
+    width: 100%;
+    margin-top: 24px;
+
+    & > li{
+        text-align: justify;
+    }
+`
+
 export default function Terms(){
     return(
-        <>Terms</>
+        <ContainerDefault>
+            <Title>Termos de uso:</Title>
+            <List>
+                <li>Os pagamentos podem ser feitos através do método Pix ou em dinheiro no momento da entrega dos produtos.</li>
+                <li>Para pagamentos via Pix, o cliente deve seguir as instruções fornecidas durante o processo de checkout.</li>
+                <li>Para pagamentos em dinheiro, o cliente deve ter o valor exato disponível no momento da entrega dos produtos. Não aceitamos cheques.</li>
+            </List>
+        </ContainerDefault>
     )
 }
